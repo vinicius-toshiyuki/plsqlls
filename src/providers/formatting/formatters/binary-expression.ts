a9e6592ede50309125a39457d01e93bceb7ca451
Vinicius Sugimoto <vtmsugimoto@gmail.com>
2025-10-22 16:59:24 -0300

    fix(formatting): spacing around operator

--- src/providers/formatting/formatters/binary-expression.ts
+++ src/providers/formatting/formatters/binary-expression.ts
@@ -1,14 +1,16 @@
 import { FormatOptions, FormatPart } from "@types";
 import { SyntaxNode } from "tree-sitter";
 import { fmtNode } from "./node";
+import { assertAtLeastOnePart } from "./util/asserts";
 
 export function fmtBinaryExpression(
   node: SyntaxNode,
   options: FormatOptions,
 ): FormatPart[] {
-  const [exp1, op, exp2] = node.children;
-
-  return fmtNode(exp1, options)
-    .concat(fmtNode(op, options))
-    .concat(fmtNode(exp2, options));
+  return node.children.flatMap((child) => {
+    const parts = fmtNode(child, options);
+    assertAtLeastOnePart(parts);
+    parts[parts.length - 1].spaceAfter = !!child.nextSibling;
+    return parts;
+  });
 }
